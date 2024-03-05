fab3e62317ed7ffe0c887482a9ee7cd121c087ab
Paul Rayner <paul@virtualgenius.com>
2024-03-04 17:09:19 -0700

    Add ID and dates to Contract

--- Test/ContractTest.cs
+++ Test/ContractTest.cs
@@ -9,7 +9,12 @@ public class ContractTest
     [TestMethod]
     public void TestContractSetUpProperly()
     {
-        var contract = new Warranty.Contract(100.0);
+        var contract = new Warranty.Contract(100.0, new DateTime(2010, 5, 6), new DateTime(2010, 5, 8), new DateTime(2013, 5, 8));
+
+        Assert.IsNotNull(contract.Id);
         Assert.AreEqual(100.0, contract.PurchasePrice);
+        Assert.AreEqual(new DateTime(2010, 5, 6), contract.PurchaseDate);
+        Assert.AreEqual(new DateTime(2010, 5, 8), contract.EffectiveDate);
+        Assert.AreEqual(new DateTime(2013, 5, 8), contract.ExpirationDate);
     }
 }
--- Warranty/Contract.cs
+++ Warranty/Contract.cs
@@ -1,12 +1,22 @@
-﻿namespace Warranty;
+﻿
+namespace Warranty;
 
 public class Contract
 {
 
     public double PurchasePrice;
 
-    public Contract(double purchasePrice) 
+    public DateTime PurchaseDate { get; }
+    public DateTime EffectiveDate { get; }
+    public DateTime ExpirationDate { get; }
+    public Guid Id { get; }
+
+    public Contract(double purchasePrice, DateTime purchaseDate, DateTime effectiveDate, DateTime expirationDate)
     {
+        Id = Guid.NewGuid();
         PurchasePrice = purchasePrice;
+        PurchaseDate = purchaseDate;
+        EffectiveDate = effectiveDate;
+        ExpirationDate = expirationDate;
     }
 }
